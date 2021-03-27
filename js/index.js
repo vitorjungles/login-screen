9f19f0f178b4da2723955d4367cefcec19c5d889
Vítor Jungles <vitorcamargojungles@gmail.com>
2021-03-27 20:12:37 -0300

    Project inicialized

--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
 var year = document.createElement("span");
 year.textContent = ` ${new Date().getFullYear()}`;
+document.querySelector("#copyright").after(year);
+
+var Create = document.querySelector("#create");
+var Validate = document.querySelector("#validate");
